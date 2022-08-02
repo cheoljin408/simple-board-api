f50c141bbdef2174160ac8c15b9977d0d6599b1e
cheoljin408 <kcj409@naver.com>
2022-08-02 18:38:09 +0900

    feat: entity 수정

--- src/main/java/com/example/simpleboardapi/domain/Comment.java
+++ src/main/java/com/example/simpleboardapi/domain/Comment.java
@@ -16,7 +16,7 @@ public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "comment_id")
-    private Integer id;
+    private Long id;
 
     @Column(columnDefinition = "TEXT")
     private String content;
