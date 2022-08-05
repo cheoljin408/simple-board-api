a4da80866f5d869750d839dc3d0bbd6abfb7859e
cheoljin408 <kcj409@naver.com>
2022-08-05 11:38:50 +0900

    feat: 전체 게시글 조회 api 구현

--- src/main/java/com/example/simpleboardapi/controller/PostController.java
+++ src/main/java/com/example/simpleboardapi/controller/PostController.java
@@ -2,18 +2,19 @@ package com.example.simpleboardapi.controller;
 
 import com.example.simpleboardapi.domain.Post;
 import com.example.simpleboardapi.dto.common.ResponseSavedIdDto;
+import com.example.simpleboardapi.dto.post.PostDto;
 import com.example.simpleboardapi.dto.post.RequestCreatePostDto;
+import com.example.simpleboardapi.dto.post.ResponsePostListDto;
 import com.example.simpleboardapi.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +22,24 @@ import java.net.URI;
 public class PostController {
 
     private final PostService postService;
+
+    @GetMapping
+    public ResponseEntity<?> postList() {
+        List<Post> postList = postService.getPostList();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ResponsePostListDto.builder()
+                        .postList(postList.stream().map(
+                                post -> PostDto.builder()
+                                        .id(post.getId())
+                                        .subject(post.getSubject())
+                                        .content(post.getContent())
+                                        .createdDate(post.getCreatedDate())
+                                        .build()
+                        ).collect(Collectors.toList())).build()
+                );
+    }
 
     @PostMapping
     public ResponseEntity<?> createPost(@RequestBody RequestCreatePostDto requestDto) {
@@ -33,6 +52,7 @@ public class PostController {
                 .location(location)
                 .body(ResponseSavedIdDto.builder()
                         .savedId(savedPost.getId())
-                        .build());
+                        .build()
+                );
     }
 }
--- src/main/java/com/example/simpleboardapi/service/PostService.java
+++ src/main/java/com/example/simpleboardapi/service/PostService.java
@@ -5,11 +5,17 @@ import com.example.simpleboardapi.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class PostService {
 
     private final PostRepository postRepository;
+
+    public List<Post> getPostList() {
+        return postRepository.findAll();
+    }
 
     public Post create(String subject, String content) {
         Post post = new Post();
